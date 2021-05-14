7cca0bab4d145ddd6bbd2399d7eadd2e63c07e76
DavidRinSE <davidrichardsonin@gmail.com>
2021-05-14 16:03:03 -0400

    Added script with parameters

---
+++ create.sh
@@ -0,0 +1,35 @@
+# ProjectName -> Used to name resources
+# Repo -> Path to repo in GitHub | "userName/repoName"
+# Branch -> Branch to watch and build | "main" | default = "main"
+# ConnectionArn -> AWS Connection Arn to GitHub account
+projectName=
+repo=
+branch=
+connectionArn=
+
+if [ -z "$projectName" ]; then
+    echo "Project name: "
+        read projectName
+fi
+
+if [ -z "$repo" ]; then
+    echo "Repo path: "
+        read repo
+fi
+
+if [ -z "$connectionArn" ]; then
+    echo "Connection Arn: "
+        read connectionArn
+fi
+
+if [ -z "$branch" ]; then
+    branch="main"
+fi
+
+echo "Outputting parameters for testing..."
+echo "Project name: $projectName"
+echo "Repo path: $repo"
+echo "Connection Arn: $connectionArn"
+echo "Branch: $branch"
+
+aws cloudformation create-stack --stack-name serverlesstest --template-body file://ServerlessCICD.yml --parameters ParameterKey=ProjectName,ParameterValue=$projectName ParameterKey=Repo,ParameterValue=$repo ParameterKey=Branch,ParameterValue=$branch ParameterKey=ConnectionArn,ParameterValue=$connectionArn --capabilities CAPABILITY_IAM \
